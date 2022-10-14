978cb22ea65fbef721e21d7db90ec2bb136ba650
luongphungnham <100785756+luongphungnham@users.noreply.github.com>
2022-10-14 19:35:15 +0700

    the sixth

--- src/uet/oop/bomberman/entities/Items/Flame.java
+++ src/uet/oop/bomberman/entities/Items/Flame.java
@@ -3,20 +3,20 @@ package uet.oop.bomberman.entities.Items;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.graphics.Sprite;
-import static uet.oop.bomberman.BombermanGame.*;
 
-public class Speed extends Items{
-    public static int speed = 10;
+import static uet.oop.bomberman.BombermanGame.fixedEntities;
+import static uet.oop.bomberman.BombermanGame.idObjects;
 
-    public Speed(int x, int y, Image img) {
+public class Flame extends Items {
+
+    public Flame() {
+
+    }
+    public Flame(int x, int y, Image img) {
         super(x, y, img);
     }
 
-    public Speed() {
-
-    }
-
-    public Speed(boolean receive) {
+    public Flame(boolean receive) {
         super(receive);
     }
 
@@ -25,13 +25,13 @@ public class Speed extends Items{
         for (Entity entity : fixedEntities)
             if (entity instanceof Speed && !this.receive)
                 if (idObjects[getX() * 16][entity.getY() * 16] == 2)
-                    entity.setImg(Sprite.powerup_speed.getFxImage());
+                    entity.setImg(Sprite.powerup_flames.getFxImage());
 
         if (!this.receive)
             if (getX() == x && getY() == y) {
                 this.setImg(Sprite.grass.getFxImage());
                 this.receive = true;
-                speed = 10;
             }
     }
 }
+
